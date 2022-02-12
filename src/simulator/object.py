aaebd46a26af40b80f303dbfff0fa17e26d79b36
Mohammad <md.kameli@gmail.com>
2022-02-12 16:09:32 +0100

    the raw files of robot,vacuum,sensor and bumbersensor added

--- src/simulator/object.py
+++ src/simulator/object.py
@@ -1,38 +1,39 @@
-#from xmlrpc.client import boolean
-#from sqlalchemy import false
+# from xmlrpc.client import boolean
+# from sqlalchemy import false
 
 # postponed evaluation of annotations. With python 3.11, we won't need this
 from __future__ import annotations
 from shape import Shape
 from position import Position
 
-ObjectId = int # type aliasing, since we might change the composition of ObjectId in future
+ObjectId = int  # type aliasing, since we might change the composition of ObjectId in future
+
 
 class Object:
     def __init__(self, id: ObjectId, shape: Shape, position: Position, owner_object):
-        #TODO can't type hint owner_object because it doesn't know Object yet :D
+        # TODO can't type hint owner_object because it doesn't know Object yet :D
         self.id = id
         self.shape = shape
-        #shape could be None
+        # shape could be None
         self.position = position
         self.owner_object = owner_object
         self.dependent_objects = []
-    
-    def evolve(self, delta_t: int, intersection_result ) -> dict[ObjectId, Object]:
-        #TODO type of intersection rejsult
-        #TODO saeed: could this be the default evolve method?
+
+    def evolve(self, delta_t: int, intersection_result) -> dict[ObjectId, Object]:
+        # TODO type of intersection result
+        # TODO saeed: could this be the default evolve method?
         collected_offspring_objects = []
 
         for object in self.dependent_objects:
             offspring_objects = object.evolve(delta_t, intersection_result)
             collected_offspring_objects.extend(offspring_objects)
-        
+
         self.dependent_objects.extend(collected_offspring_objects)
         return collected_offspring_objects
-    
+
     def visualize(self):
         pass
-    
+
     def bounding_box(self):
         pass
 
@@ -41,6 +42,6 @@ class Object:
            returns 0 if the objects declares no requirement.
         """
         return 0
-        
+
     def time_to_die(self) -> bool:
         return False
